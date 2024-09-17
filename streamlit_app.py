3040d950b73ad5de3874bebf12ab725048c479f5
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-17 12:59:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ import pandas as pd
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/population-dashboard/master/data/us-population-2010-2019-reshaped.csv")
+df
